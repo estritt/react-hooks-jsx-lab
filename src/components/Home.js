8f297c3bfffa1aafb54f2b226f8c1e5ba19c2139
Genevieve Strittmatter <eastrittmatter@gmail.com>
2024-01-30 10:55:31 -0500

    completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,10 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  // style has one set of curly braces because it takes an object and another to evaluate an expression
+  return <div id="home">
+    <h1 style={{color: "firebrick"}}>{name} is a Web Developer from {city}</h1>
+  </div>;
 }
 
 export default Home;
